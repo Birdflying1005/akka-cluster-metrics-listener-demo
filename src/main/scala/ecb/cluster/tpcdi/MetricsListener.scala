195a4d73a44012907c8ab6d8d64f9fd9dd7841e5
Ben Hadley <bhadley2@gmail.com>
2016-10-29 16:34:50 -0700

    Add address logic and reduce number of seeds by one

--- src/main/scala/ecb/cluster/tpcdi/MetricsListener.scala
+++ src/main/scala/ecb/cluster/tpcdi/MetricsListener.scala
@@ -2,6 +2,7 @@ package ecb.cluster.tpcdi
 
 import akka.actor.ActorLogging
 import akka.actor.Actor
+import akka.actor.AddressFromURIString
 import akka.cluster.Cluster
 import akka.cluster.metrics.ClusterMetricsEvent
 import akka.cluster.metrics.ClusterMetricsChanged
@@ -10,6 +11,7 @@ import akka.cluster.metrics.NodeMetrics
 import akka.cluster.metrics.StandardMetrics.HeapMemory
 import akka.cluster.metrics.StandardMetrics.Cpu
 import akka.cluster.metrics.ClusterMetricsExtension
+import java.net.URLEncoder
      
 class MetricsListener extends Actor with ActorLogging {
   val selfAddress = Cluster(context.system).selfAddress
@@ -21,7 +23,7 @@ class MetricsListener extends Actor with ActorLogging {
      
   def receive = {
     case ClusterMetricsChanged(clusterMetrics) =>
-      clusterMetrics.filter(_.address == selfAddress) foreach { nodeMetrics =>
+      clusterMetrics.filter(_.address == AddressFromURIString("akka.tcp://tpcdi@127.0.0.1:2551")) foreach { nodeMetrics =>
         logHeap(nodeMetrics)
         logCpu(nodeMetrics)
       }
@@ -30,13 +32,13 @@ class MetricsListener extends Actor with ActorLogging {
      
   def logHeap(nodeMetrics: NodeMetrics): Unit = nodeMetrics match {
     case HeapMemory(address, timestamp, used, committed, max) =>
-      log.info("Used heap: {} MB", used.doubleValue / 1024 / 1024)
+      log.info("{} Used heap: {} MB", address, used.doubleValue / 1024 / 1024)
     case _ => // No heap info.
   }
      
   def logCpu(nodeMetrics: NodeMetrics): Unit = nodeMetrics match {
     case Cpu(address, timestamp, Some(systemLoadAverage), cpuCombined, cpuStolen, processors) =>
-      log.info("Load: {} ({} processors)", systemLoadAverage, processors)
+      log.info("{} Load: {} ({} processors)", address, systemLoadAverage, processors)
     case _ => // No cpu info.
   }
 }
